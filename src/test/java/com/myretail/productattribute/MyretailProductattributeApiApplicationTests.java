bcc49400ff382e904e43f135e16ad0646b26455a
bimlendushekhargit <bimlendu.shekhar.ind@gmail.com>
2020-09-10 13:51:19 +0530

    sonar

--- src/test/java/com/myretail/productattribute/MyretailProductattributeApiApplicationTests.java
+++ src/test/java/com/myretail/productattribute/MyretailProductattributeApiApplicationTests.java
@@ -1,13 +1,28 @@
 package com.myretail.productattribute;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MyretailProductattributeApiApplicationTests {
 
+	@Autowired
+	private ProductAttributeController controller;
+
 	@Test
-	void contextLoads() {
+	public void contexLoads() throws Exception {
+		assertThat(controller).isNotNull();
+	}
+	
+	@Test
+	public void testPriceResponse() throws Exception {
+		Optional<Product> product = controller.findProductAttribute("1");
+		assertThat(product.get().getAttribute()).isNotNull();
 	}
 
 }
